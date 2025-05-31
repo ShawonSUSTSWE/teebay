4f4e8bfc253b6d2482cf2581d089d25ae7f0ecae
ShawonSUSTSWE <shawon2046@gmail.com>
2025-05-31 10:02:06 +0600

    Initial Server Setup with Prisma and GraphQL Done

--- server/index.js
+++ server/index.js
@@ -1,18 +1,28 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import { ApolloServer } from "@apollo/server";
+import { PrismaClient } from "@prisma/client";
+import { typeDefs } from "./src/schema/typeDefs.js";
+import { resolvers } from "./src/schema/resolvers.js";
+import { expressMiddleware } from "@apollo/server/express4";
 dotenv.config();
 
+const prisma = new PrismaClient();
 const PORT = process.env.PORT || 8000;
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Welcome to Teebay!");
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: () => ({ prisma }),
 });
 
+await server.start();
+app.use("/graphql", expressMiddleware(server));
+
 app.listen(PORT, () => {
-  console.log(`Server is running on port http://localhost:${PORT}`);
+  console.log(`Server is running on port http://localhost:${PORT}/graphql`);
 });

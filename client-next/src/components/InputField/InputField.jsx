72cbeeaeb2a0664069221860d6edc01dbaeb89ee
ShawonSUSTSWE <shawon2046@gmail.com>
2025-06-01 13:27:08 +0600

    Input Field Component Added

---
+++ client-next/src/components/InputField/InputField.jsx
@@ -0,0 +1,21 @@
+import { getClassNames } from "@/lib/utils/commonUtils";
+import styles from "./InputField.module.css";
+import Input from "@/components/Input/Input";
+
+const classNames = getClassNames(styles);
+
+export default function InputField({
+  label,
+  error,
+  showError = true,
+  className,
+  ...inputProps
+}) {
+  console.log(error);
+  return (
+    <div className={classNames("input-field", className)}>
+      <Input {...inputProps} error={error} />
+      {error && showError && <div className={classNames("error")}>{error}</div>}
+    </div>
+  );
+}

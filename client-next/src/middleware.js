98215a64c7365041bd09a13b032ed75882a0111f
ShawonSUSTSWE <shawon2046@gmail.com>
2025-06-01 12:09:01 +0600

    Middleware modified

--- client-next/src/middleware.js
+++ client-next/src/middleware.js
@@ -1,25 +1,36 @@
 import { NextResponse } from "next/server";
 
-const PUBLIC_ROUTES = ["/login", "/signup", "/about"];
+const AUTH_ROUTES = ["/login", "/signup"];
+
+const PUBLIC_ROUTES = ["/", ...AUTH_ROUTES];
+
+const redirectToPath = (path, baseUrl) => {
+  const url = basePath;
+  url.pathname = path;
+  return NextResponse.redirect(url);
+};
 
 export async function middleware(req) {
   const { pathname } = req.nextUrl;
+
+  const token = req.cookies.get("token")?.value || null;
+  const basePath = req.nextUrl.clone();
+
+  if (token && AUTH_ROUTES.includes(pathname)) {
+    return redirectToPath("/", basePath);
+  }
 
   if (PUBLIC_ROUTES.includes(pathname)) {
     return NextResponse.next();
   }
 
-  const token = req.cookies.get("token")?.value;
-
   if (!token) {
-    const url = req.nextUrl.clone();
-    url.pathname = "/login";
-    return NextResponse.redirect(url);
+    return redirectToPath("/login", basePath);
   }
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/", "/dashboard/:path*", "/profile/:path*"],
+  matcher: ["/"],
 };
